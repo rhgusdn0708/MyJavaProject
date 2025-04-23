2d7f3ef66a7a5a393adc88fee5647911c1c6e637
rhgusdn0708 <hu070821@gmail.com>
2025-04-23 17:47:29 +0900

    book

---
+++ src/workshop/book/entity/Magazine.java
@@ -0,0 +1,27 @@
+package workshop.book.entity;
+
+public class Magazine extends Publication{
+	private String publishingPeriod;
+	
+	public Magazine() {
+		
+	}
+
+	public Magazine(String title, String publishingDate, int page, int price, String publishingPeriod) {
+		super(title, publishingDate, page, price);
+		this.publishingPeriod = publishingPeriod;
+	}
+	
+	public String getPublishingPeriod() {
+		return publishingPeriod;
+	}
+	
+	public void setPublishingPeriod(String publishingPeriod) {
+		this.publishingPeriod = publishingPeriod;
+	}
+	
+	public String getPublishingPreriod() {
+		return publishingPeriod;
+	}
+
+}
--- src/workshop/person/control/PersonManager.java
+++ src/workshop/person/control/PersonManager.java
@@ -19,6 +19,8 @@ public class PersonManager {
 		mgr.showPersons(persons);
 		
 		System.out.println(mgr.findByGender(persons, '남'));
+		
+		mgr.showPerson(persons, "김하늘");
 				
 	}
 
@@ -48,11 +50,27 @@ public class PersonManager {
 	public int findByGender(PersonEntity[] persons, char gender) {
 		 int genderCnt = 0;
 		 for (PersonEntity person : persons) {
-//			if(person.getGender())
-			 System.out.println(person.getGender());
+ 			 if(person.getGender() == gender) {
+ 				 genderCnt++;
+ 			 }
 		}
 		 return genderCnt;
 	}
+	
+	public void showPerson(PersonEntity[] persons, String name) {
+		for (PersonEntity person : persons) {
+			//String은 reference 타입이므로 값을 비교할때 eqauls() 메서드를 사용해야 함
+			if(person.getName().equals(name)) {
+				System.out.println("[이름] " + person.getName());
+				System.out.println("[성별] " + person.getGender());
+				System.out.println("[전화번호] " + person.getPhone());
+				System.out.println("[주소] " + person.getAddress());
+				break;
+			}
+		}
+	}
+	
+	
 		
 }
 
