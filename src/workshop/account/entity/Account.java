1cad31ab89b0e46a5fefb51e279e3a7f5cb82037
rhgusdn0708 <hu070821@gmail.com>
2025-04-23 11:16:41 +0900

    Account method add

--- src/workshop/account/entity/Account.java
+++ src/workshop/account/entity/Account.java
@@ -1,5 +1,42 @@
 package workshop.account.entity;
 
 public class Account {
-
+	private String custId;
+	private String acctId;
+	private int balance;
+	
+	//setter
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+	public void setAcctId(String acctId) {
+		this.acctId = acctId;
+	}
+//	public void setBalance(int balance) {
+//		this.balance = balance;
+//	}
+	
+	//getter
+	public int getBalance() {
+		return balance;
+	}
+	public String getCustId() {
+		return custId;
+	}
+	public String getAcctId() {
+		return acctId;
+	}
+	
+	//입금
+	public void deposit(int amount) {
+		this.balance += amount;
+	}
+	//출금
+	public void withdraw(int amount) {
+		if(amount > balance) {
+			System.out.println("ÀÜ¾×ºÎÁ·");
+		}
+		this.balance -= amount;
+	}
+	
 }
