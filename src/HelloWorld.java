99269310e967c835ca261d98d051f32cbfc3bcd5
rhgusdn0708 <hu070821@gmail.com>
2025-04-23 10:52:11 +0900

    java project

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+/*
+ * javac(자바수동컴파일) HelloWorld.java
+ * java HelloWorld
+ * Run 단축키 : Ctrl + F11
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+}
