2cc245ab683fcc0f15d46831308ca11a2a24a94e
rhgusdn0708 <hu070821@gmail.com>
2025-04-24 11:29:19 +0900

    Animal

---
+++ src/workshop/anmial/entity/Animal.java
@@ -0,0 +1,17 @@
+package workshop.anmial.entity;
+
+public abstract class Animal {
+	protected int legs;
+	
+	protected Animal(int legs) {
+		this.legs = legs;
+		
+	}
+	
+	public abstract void eat();
+	
+	public void wark() {
+		System.out.println("동물은 " + legs +  "발로 걷는다.");
+	}
+	
+}
---
+++ src/workshop/anmial/entity/Pet.java
@@ -0,0 +1,7 @@
+package workshop.anmial.entity;
+
+public interface Pet {
+	String getName();
+	void setName(String name);
+	public abstract void play();
+}
